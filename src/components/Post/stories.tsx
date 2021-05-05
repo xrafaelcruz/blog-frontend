d4ebf494cf90bbbcc1749446e95815f5aff15d86
Rafael Cruz <xrafaelcruz@gmail.com>
2021-05-04 22:41:07 -0300

    fixed storybook to work with absolute path

--- src/components/Post/stories.tsx
+++ src/components/Post/stories.tsx
@@ -6,9 +6,13 @@ export default {
   title: 'Post',
   component: Post,
   args: {
-    title: 'Rafael Cruz',
+    title: 'Post1',
     createdAt: '2021-05-01T18:58:27.880Z'
   }
 } as Meta
 
-export const Default: Story<PropTypes> = (args) => <Post {...args} />
+export const Default: Story<PropTypes> = (args) => (
+  <div style={{ background: '#000', padding: '50px' }}>
+    <Post {...args} />
+  </div>
+)
