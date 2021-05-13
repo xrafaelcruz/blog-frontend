c380798930741b276b6ee686c98055c4ef08d3de
Rafael Cruz <xrafaelcruz@gmail.com>
2021-05-12 21:34:36 -0300

    added Author component style

---
+++ src/components/Author/style.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+import HTML from 'components/HTML'
+
+export const Title = styled.h1`
+  text-align: center;
+`
+
+export const Photo = styled.img`
+  border-radius: 50%;
+  height: 200px;
+  margin: 2rem auto;
+  object-fit: cover;
+  width: 200px;
+`
+
+export const Description = styled.h3`
+  text-align: center;
+`
+
+export const Content = styled(HTML)`
+  margin-top: 8rem;
+  max-width: 100%;
+`
