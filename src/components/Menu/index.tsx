7f5bd7ebd3d7c91bc288d0ac98c638a35fa8c139
Rafael Cruz <xrafaelcruz@gmail.com>
2021-06-20 20:54:50 -0300

    undone some menu changes

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,21 @@
+import Link from 'next/link'
+
 import useToggle from 'utils/hooks/toggle'
 
 import Button from './Button'
-import Items from './Items'
 
 import * as S from './style'
+
+const items = [
+  {
+    label: 'Home',
+    href: '/'
+  },
+  {
+    label: 'Sobre',
+    href: '/sobre'
+  }
+]
 
 const Menu = () => {
   const [active, toggle] = useToggle()
@@ -11,7 +23,14 @@ const Menu = () => {
   return (
     <S.Wrapper>
       <Button onClick={toggle} active={`${active}`} />
-      <Items onClick={toggle} active={`${active}`} />
+
+      <S.Nav active={`${active}`}>
+        {items.map((item, i) => (
+          <Link href={item.href} key={i}>
+            <a onClick={toggle}>{item.label}</a>
+          </Link>
+        ))}
+      </S.Nav>
     </S.Wrapper>
   )
 }
--- src/components/Menu/style.ts
+++ src/components/Menu/style.ts
@@ -1,6 +1,55 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+type NavProps = {
+  active: string
+}
 
 export const Wrapper = styled.div`
   height: 48px;
   width: 48px;
+`
+
+const navActive = css`
+  opacity: 1;
+  pointer-events: auto;
+  transform: translate3D(0, 0, 0);
+  visibility: visible;
+`
+
+export const Nav = styled.nav<NavProps>`
+  align-items: center;
+  background: ${(props) => props.theme.color.secondary};
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+  justify-content: center;
+  height: 100vh;
+  left: 0;
+  opacity: 0;
+  padding: 64px 16px;
+  position: fixed;
+  pointer-events: none;
+  top: 0;
+  transform: translate3D(0, -50px, 0);
+  transition: 0.3s;
+  visibility: hidden;
+  width: 100vw;
+  z-index: 1;
+
+  ${(props) => props.active === 'true' && navActive}
+
+  a {
+    color: ${(props) => props.theme.color.light};
+    font-size: 4rem;
+    text-transform: uppercase;
+    transition: all 0.3s ease;
+
+    &:hover {
+      color: ${(props) => props.theme.color.primary};
+    }
+
+    @media (max-width: 920px) {
+      font-size: 3.8rem;
+    }
+  }
 `
