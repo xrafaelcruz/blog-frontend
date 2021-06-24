6e910ede07cdbff0ab706741a8d9fe4a5d072fcd
Rafael Cruz <xrafaelcruz@gmail.com>
2021-06-24 20:32:08 -0300

    uncommented responsive tests to Menu

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -9,8 +9,10 @@ describe('<Menu />', () => {
   const media = '(max-width: 920px)'
 
   it('should apply styles to the links on mobile', () => {
-    // renderWithTheme(Component)
-    // const Link = screen.getByTestId('Home')
-    // expect(Link).toHaveStyleRule('font-size', '3.8rem', { media })
+    renderWithTheme(Component)
+
+    const Link = screen.getByTestId('Home')
+
+    expect(Link).toHaveStyleRule('font-size', '3.8rem', { media })
   })
 })
